de2f87fca177d34db0910c47dc9cc052d371db00
Soumyajyoti Mahalanobish <soumyajyotimahalanobish@Soumyajyotis-MacBook-Air.local>
2024-04-14 11:22:27 +0530

    POST request

--- just-an-api/README.md
+++ just-an-api/README.md
@@ -3,3 +3,5 @@ Framework: gin
 go mod init
 
 go get github.com/gin-gonic/gin
+
+curl localhost:8080/books --include --header "Content-Type: application/json" -d @body.json --request "POST"
--- just-an-api/main.go
+++ just-an-api/main.go
@@ -1,9 +1,43 @@
 package main
 
 import (
-	"fmt"
 	"net/http"
+
 	"github.com/gin-gonic/gin"
-	"errors"
 )
 
+type book struct {
+	ID       string `json:"id"`
+	Title    string `json:"title"`
+	Author   string `json:"author"`
+	Quantity int    `json:"quantity"`
+}
+
+var books = []book{
+	{ID: "1", Title: "In Search of Lost Time", Author: "Marcel Proust", Quantity: 2},
+	{ID: "2", Title: "The Great Gatsby", Author: "F. Scott Fitzgerald", Quantity: 5},
+	{ID: "3", Title: "War and Peace", Author: "Leo Tolstoy", Quantity: 6},
+}
+
+func getBooks(c *gin.Context) { // -> function to return get requests for books
+	c.IndentedJSON(http.StatusOK, books)
+}
+
+func createBook(c *gin.Context){ // -> function to create a book
+	var newBook book
+
+	if err := c.Bind(&newBook); err != nil{ //  sort of error handling for the user
+		return
+	}
+
+	books=append(books, newBook)
+	c.IndentedJSON(http.StatusCreated, newBook)
+
+}
+
+func main() {
+	router := gin.Default() // -> router for handling different routes
+	router.GET("/books", getBooks)
+	router.POST("/books", createBook)
+	router.Run("localhost:8080")
+}
